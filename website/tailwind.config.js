86c856f6dc98e718c1104d7f209fefb72b4f7778
Anawat <66070217@kmitl.ac.th>
2024-10-20 08:12:37 +0700

    correct theme add some font and add about page

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        EmbedCode: ['"Playwrite GB S"', "cursive"],
+      },
+    },
   },
   plugins: [],
 }
